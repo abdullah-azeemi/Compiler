5448b4d94d902da5aa762652c9518d010ad06fd1
Abdullah <abdullahazeemi200@gmail.com>
2025-09-28 18:04:42 +0500

    ast ptr fix

--- Utilities/ast_printer.hpp
+++ Utilities/ast_printer.hpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
 #include "../ast.hpp"
 #include "token_types.hpp"
 
@@ -13,7 +15,12 @@ class ASTPrinter
 private:
   static string indent(int level)
   {
-    return string(level * 4, ' ');
+    string result = "";
+    for (int i = 0; i < level * 4; i++)
+    {
+      result += " ";
+    }
+    return result;
   }
 
   static string tokenTypeToString(TokenType type)
@@ -70,7 +77,7 @@ private:
     }
   }
 
-  static void printExpr(const ExprPtr &expr, int level, ostringstream &out)
+  static void printExpr(Expr *expr, int level, ostringstream &out)
   {
     if (!expr)
     {
@@ -78,211 +85,326 @@ private:
       return;
     }
 
-    std::visit([&](const auto &e)
-               {
-            using T = std::decay_t<decltype(e)>;
-            
-            if constexpr (std::is_same_v<T, IntLiteral>) {
-                out << e.value;
-            }
-            else if constexpr (std::is_same_v<T, FloatLiteral>) {
-                out << e.value;
-            }
-            else if constexpr (std::is_same_v<T, StringLiteral>) {
-                out << "\"" << e.value << "\"";
-            }
-            else if constexpr (std::is_same_v<T, BoolLiteral>) {
-                out << (e.value ? "true" : "false");
-            }
-            else if constexpr (std::is_same_v<T, Identifier>) {
-                out << "\"" << e.name << "\"";
-            }
-            else if constexpr (std::is_same_v<T, BinaryOp>) {
-                out << "::" << tokenTypeToString(e.op) << "\n";
-                out << indent(level + 1);
-                printExpr(e.left, level + 1, out);
-                out << "\n" << indent(level + 1);
-                printExpr(e.right, level + 1, out);
-            }
-            else if constexpr (std::is_same_v<T, UnaryOp>) {
-                out << "Unary(" << tokenTypeToString(e.op) << ")\n";
-                out << indent(level + 1);
-                printExpr(e.operand, level + 1, out);
-            }
-            else if constexpr (std::is_same_v<T, Assignment>) {
-                out << "Assign(" << tokenTypeToString(T_ASSIGNOP_RL) << ")\n";
-                out << indent(level + 1) << "\"" << e.ident << "\"\n";
-                out << indent(level + 1);
-                printExpr(e.value, level + 1, out);
-            }
-            else if constexpr (std::is_same_v<T, FunctionCall>) {
-                out << "Call(FnCall {\n";
-                out << indent(level + 1) << "ident: \"" << e.name << "\",\n";
-                out << indent(level + 1) << "args: [\n";
-                for (const auto& arg : e.args) {
-                    out << indent(level + 2) << "Some(\n";
-                    out << indent(level + 3);
-                    printExpr(arg, level + 3, out);
-                    out << ",\n" << indent(level + 2) << "),\n";
-                }
-                out << indent(level + 1) << "],\n";
-                out << indent(level) << "})";
-            } }, expr->value);
+    switch (expr->nodeType)
+    {
+    case NODE_INT_LIT:
+    {
+      IntLiteral *lit = (IntLiteral *)expr;
+      out << lit->value;
+      break;
+    }
+    case NODE_FLOAT_LIT:
+    {
+      FloatLiteral *lit = (FloatLiteral *)expr;
+      out << lit->value;
+      break;
+    }
+    case NODE_STRING_LIT:
+    {
+      StringLiteral *lit = (StringLiteral *)expr;
+      out << "\"" << lit->value << "\"";
+      break;
+    }
+    case NODE_BOOL_LIT:
+    {
+      BoolLiteral *lit = (BoolLiteral *)expr;
+      out << (lit->value ? "true" : "false");
+      break;
+    }
+    case NODE_IDENTIFIER:
+    {
+      Identifier *id = (Identifier *)expr;
+      out << "\"" << id->name << "\"";
+      break;
+    }
+    case NODE_BINARY_OP:
+    {
+      BinaryOp *op = (BinaryOp *)expr;
+      out << "::" << tokenTypeToString(op->op) << "\n";
+      out << indent(level + 1);
+      printExpr(op->left, level + 1, out);
+      out << "\n"
+          << indent(level + 1);
+      printExpr(op->right, level + 1, out);
+      break;
+    }
+    case NODE_UNARY_OP:
+    {
+      UnaryOp *op = (UnaryOp *)expr;
+      out << "Unary(" << tokenTypeToString(op->op) << ")\n";
+      out << indent(level + 1);
+      printExpr(op->operand, level + 1, out);
+      break;
+    }
+    case NODE_ASSIGNMENT:
+    {
+      Assignment *assign = (Assignment *)expr;
+      out << "Assign(" << tokenTypeToString(T_ASSIGNOP_RL) << ")\n";
+      out << indent(level + 1) << "\"" << assign->ident << "\"\n";
+      out << indent(level + 1);
+      printExpr(assign->value, level + 1, out);
+      break;
+    }
+    case NODE_FUNC_CALL:
+    {
+      FunctionCall *call = (FunctionCall *)expr;
+      out << "Call(FnCall {\n";
+      out << indent(level + 1) << "ident: \"" << call->name << "\",\n";
+      out << indent(level + 1) << "args: [\n";
+      for (auto arg : call->args)
+      {
+        out << indent(level + 2) << "Some(\n";
+        out << indent(level + 3);
+        printExpr(arg, level + 3, out);
+        out << ",\n"
+            << indent(level + 2) << "),\n";
+      }
+      out << indent(level + 1) << "],\n";
+      out << indent(level) << "})";
+      break;
+    }
+    }
   }
 
-  static void printStmt(const StmtPtr &stmt, int level, std::ostringstream &out)
+  static void printStmt(Stmt *stmt, int level, ostringstream &out)
   {
     if (!stmt)
       return;
 
-    visit([&](const auto &s)
-          {
-            using T = std::decay_t<decltype(s)>;
-            
-            if constexpr (std::is_same_v<T, FunctionDecl>) {
-                out << indent(level) << "Fn(\n";
-                out << indent(level + 1) << "FnDecl {\n";
-                out << indent(level + 2) << "type_tok: " << tokenTypeToString(s.returnType) << ",\n";
-                out << indent(level + 2) << "ident: \"" << s.name << "\",\n";
-                out << indent(level + 2) << "params: [\n";
-                for (const auto& param : s.params) {
-                    out << indent(level + 3) << "Param {\n";
-                    out << indent(level + 4) << "type_tok: " << tokenTypeToString(param.type) << ",\n";
-                    out << indent(level + 4) << "ident: \"" << param.name << "\",\n";
-                    out << indent(level + 3) << "},\n";
-                }
-                out << indent(level + 2) << "],\n";
-                out << indent(level + 2) << "block: [\n";
-                printStmt(s.body, level + 3, out);
-                out << indent(level + 2) << "],\n";
-                out << indent(level + 1) << "},\n";
-                out << indent(level) << "),\n";
-            }
-            else if constexpr (std::is_same_v<T, VarDecl>) {
-                out << indent(level) << "Var(\n";
-                out << indent(level + 1) << "VarDecl {\n";
-                out << indent(level + 2) << "type_tok: " << tokenTypeToString(s.type) << ",\n";
-                out << indent(level + 2) << "ident: \"" << s.ident << "\",\n";
-                out << indent(level + 2) << "expr: ";
-                if (s.expr) {
-                    out << "Some(\n" << indent(level + 3);
-                    printExpr(s.expr, level + 3, out);
-                    out << ",\n" << indent(level + 2) << ")";
-                } else {
-                    out << "None";
-                }
-                out << ",\n";
-                out << indent(level + 1) << "},\n";
-                out << indent(level) << "),\n";
-            }
-            else if constexpr (std::is_same_v<T, Block>) {
-                for (const auto& stmt : s.stmts) {
-                    printStmt(stmt, level, out);
-                }
-            }
-            else if constexpr (std::is_same_v<T, ReturnStmt>) {
-                out << indent(level) << "Ret(\n";
-                out << indent(level + 1) << "ExprStmt {\n";
-                out << indent(level + 2) << "expr: ";
-                if (s.expr) {
-                    out << "Some(\n" << indent(level + 3);
-                    printExpr(s.expr, level + 3, out);
-                    out << "\n" << indent(level + 2) << "),\n";
-                } else {
-                    out << "None,\n";
-                }
-                out << indent(level + 1) << "},\n";
-                out << indent(level) << "),\n";
-            }
-            else if constexpr (std::is_same_v<T, IfStmt>) {
-                out << indent(level) << "If(\n";
-                out << indent(level + 1) << "IfStmt {\n";
-                out << indent(level + 2) << "cond: Some(\n";
-                out << indent(level + 3);
-                printExpr(s.condition, level + 3, out);
-                out << "\n" << indent(level + 2) << "),\n";
-                out << indent(level + 2) << "if_block: [\n";
-                printStmt(s.thenBranch, level + 3, out);
-                out << indent(level + 2) << "],\n";
-                out << indent(level + 2) << "else_block: ";
-                if (s.elseBranch) {
-                    out << "[\n";
-                    printStmt(s.elseBranch, level + 3, out);
-                    out << indent(level + 2) << "]";
-                } else {
-                    out << "[]";
-                }
-                out << ",\n";
-                out << indent(level + 1) << "},\n";
-                out << indent(level) << "),\n";
-            }
-            else if constexpr (std::is_same_v<T, ForStmt>) {
-                out << indent(level) << "For(\n";
-                out << indent(level + 1) << "ForStmt {\n";
-                out << indent(level + 2) << "init: ";
-                if (s.init) {
-                    out << "Some(\n";
-                    printStmt(s.init, level + 3, out);
-                    out << indent(level + 2) << "),\n";
-                } else {
-                    out << "None,\n";
-                }
-                out << indent(level + 2) << "cond: ExprStmt {\n";
-                out << indent(level + 3) << "expr: ";
-                if (s.condition) {
-                    out << "Some(\n" << indent(level + 4);
-                    printExpr(s.condition, level + 4, out);
-                    out << "\n" << indent(level + 3) << "),\n";
-                } else {
-                    out << "None,\n";
-                }
-                out << indent(level + 2) << "},\n";
-                out << indent(level + 2) << "updt: ";
-                if (s.update) {
-                    out << "Some(\n" << indent(level + 3);
-                    printExpr(s.update, level + 3, out);
-                    out << "\n" << indent(level + 2) << "),\n";
-                } else {
-                    out << "None,\n";
-                }
-                out << indent(level + 2) << "block: [\n";
-                printStmt(s.body, level + 3, out);
-                out << indent(level + 2) << "],\n";
-                out << indent(level + 1) << "},\n";
-                out << indent(level) << "),\n";
-            }
-            else if constexpr (std::is_same_v<T, BreakStmt>) {
-                out << indent(level) << "Break,\n";
-            }
-            else if constexpr (std::is_same_v<T, ContinueStmt>) {
-                out << indent(level) << "Continue,\n";
-            }
-            else if constexpr (std::is_same_v<T, ExprStmt>) {
-                out << indent(level) << "ExprStmt {\n";
-                out << indent(level + 1) << "expr: ";
-                if (s.expr) {
-                    out << "Some(\n" << indent(level + 2);
-                    printExpr(s.expr, level + 2, out);
-                    out << "\n" << indent(level + 1) << ")";
-                } else {
-                    out << "None";
-                }
-                out << ",\n" << indent(level) << "},\n";
-            } }, stmt->value);
+    switch (stmt->nodeType)
+    {
+    case NODE_VAR_DECL:
+    {
+      VarDecl *var = (VarDecl *)stmt;
+      out << indent(level) << "Var(\n";
+      out << indent(level + 1) << "VarDecl {\n";
+      out << indent(level + 2) << "type_tok: " << tokenTypeToString(var->type) << ",\n";
+      out << indent(level + 2) << "ident: \"" << var->ident << "\",\n";
+      out << indent(level + 2) << "expr: ";
+      if (var->expr)
+      {
+        out << "Some(\n"
+            << indent(level + 3);
+        printExpr(var->expr, level + 3, out);
+        out << ",\n"
+            << indent(level + 2) << ")";
+      }
+      else
+      {
+        out << "None";
+      }
+      out << ",\n";
+      out << indent(level + 1) << "},\n";
+      out << indent(level) << "),\n";
+      break;
+    }
+    case NODE_FUNC_DECL:
+    {
+      FunctionDecl *fn = (FunctionDecl *)stmt;
+      out << indent(level) << "Fn(\n";
+      out << indent(level + 1) << "FnDecl {\n";
+      out << indent(level + 2) << "type_tok: " << tokenTypeToString(fn->returnType) << ",\n";
+      out << indent(level + 2) << "ident: \"" << fn->name << "\",\n";
+      out << indent(level + 2) << "params: [\n";
+      for (auto &p : fn->params)
+      {
+        out << indent(level + 3) << "Param {\n";
+        out << indent(level + 4) << "type_tok: " << tokenTypeToString(p.type) << ",\n";
+        out << indent(level + 4) << "ident: \"" << p.name << "\",\n";
+        out << indent(level + 3) << "},\n";
+      }
+      out << indent(level + 2) << "],\n";
+      out << indent(level + 2) << "block: [\n";
+      printStmt(fn->body, level + 3, out);
+      out << indent(level + 2) << "],\n";
+      out << indent(level + 1) << "},\n";
+      out << indent(level) << "),\n";
+      break;
+    }
+    case NODE_BLOCK:
+    {
+      Block *block = (Block *)stmt;
+      for (auto s : block->stmts)
+      {
+        printStmt(s, level, out);
+      }
+      break;
+    }
+    case NODE_RETURN:
+    {
+      ReturnStmt *ret = (ReturnStmt *)stmt;
+      out << indent(level) << "Ret(\n";
+      out << indent(level + 1) << "ExprStmt {\n";
+      out << indent(level + 2) << "expr: ";
+      if (ret->expr)
+      {
+        out << "Some(\n"
+            << indent(level + 3);
+        printExpr(ret->expr, level + 3, out);
+        out << "\n"
+            << indent(level + 2) << "),\n";
+      }
+      else
+      {
+        out << "None,\n";
+      }
+      out << indent(level + 1) << "},\n";
+      out << indent(level) << "),\n";
+      break;
+    }
+    case NODE_IF:
+    {
+      IfStmt *ifStmt = (IfStmt *)stmt;
+      out << indent(level) << "If(\n";
+      out << indent(level + 1) << "IfStmt {\n";
+      out << indent(level + 2) << "cond: Some(\n";
+      out << indent(level + 3);
+      printExpr(ifStmt->condition, level + 3, out);
+      out << "\n"
+          << indent(level + 2) << "),\n";
+      out << indent(level + 2) << "if_block: [\n";
+      printStmt(ifStmt->thenBranch, level + 3, out);
+      out << indent(level + 2) << "],\n";
+      out << indent(level + 2) << "else_block: ";
+      if (ifStmt->elseBranch)
+      {
+        out << "[\n";
+        printStmt(ifStmt->elseBranch, level + 3, out);
+        out << indent(level + 2) << "]";
+      }
+      else
+      {
+        out << "[]";
+      }
+      out << ",\n";
+      out << indent(level + 1) << "},\n";
+      out << indent(level) << "),\n";
+      break;
+    }
+    case NODE_FOR:
+    {
+      ForStmt *forStmt = (ForStmt *)stmt;
+      out << indent(level) << "For(\n";
+      out << indent(level + 1) << "ForStmt {\n";
+      out << indent(level + 2) << "init: ";
+      if (forStmt->init)
+      {
+        out << "Some(\n";
+        printStmt(forStmt->init, level + 3, out);
+        out << indent(level + 2) << "),\n";
+      }
+      else
+      {
+        out << "None,\n";
+      }
+      out << indent(level + 2) << "cond: ExprStmt {\n";
+      out << indent(level + 3) << "expr: ";
+      if (forStmt->condition)
+      {
+        out << "Some(\n"
+            << indent(level + 4);
+        printExpr(forStmt->condition, level + 4, out);
+        out << "\n"
+            << indent(level + 3) << "),\n";
+      }
+      else
+      {
+        out << "None,\n";
+      }
+      out << indent(level + 2) << "},\n";
+      out << indent(level + 2) << "updt: ";
+      if (forStmt->update)
+      {
+        out << "Some(\n"
+            << indent(level + 3);
+        printExpr(forStmt->update, level + 3, out);
+        out << "\n"
+            << indent(level + 2) << "),\n";
+      }
+      else
+      {
+        out << "None,\n";
+      }
+      out << indent(level + 2) << "block: [\n";
+      printStmt(forStmt->body, level + 3, out);
+      out << indent(level + 2) << "],\n";
+      out << indent(level + 1) << "},\n";
+      out << indent(level) << "),\n";
+      break;
+    }
+    case NODE_WHILE:
+    {
+      WhileStmt *whileStmt = (WhileStmt *)stmt;
+      out << indent(level) << "While(\n";
+      out << indent(level + 1) << "WhileStmt {\n";
+      out << indent(level + 2) << "cond: ";
+      if (whileStmt->condition)
+      {
+        out << "Some(\n"
+            << indent(level + 3);
+        printExpr(whileStmt->condition, level + 3, out);
+        out << "\n"
+            << indent(level + 2) << "),\n";
+      }
+      else
+      {
+        out << "None,\n";
+      }
+      out << indent(level + 2) << "body: [\n";
+      printStmt(whileStmt->body, level + 3, out);
+      out << indent(level + 2) << "],\n";
+      out << indent(level + 1) << "},\n";
+      out << indent(level) << "),\n";
+      break;
+    }
+    case NODE_BREAK:
+    {
+      out << indent(level) << "Break,\n";
+      break;
+    }
+    case NODE_CONTINUE:
+    {
+      out << indent(level) << "Continue,\n";
+      break;
+    }
+    case NODE_EXPR_STMT:
+    {
+      ExprStmt *exprStmt = (ExprStmt *)stmt;
+      if (exprStmt->expr)
+      {
+        printExpr(exprStmt->expr, level, out);
+      }
+      break;
+    }
+    }
   }
 
 public:
-  static string printAST(const vector<StmtPtr> &ast)
+  static string printAST(const vector<Stmt *> &ast)
   {
     ostringstream out;
     out << "[\n";
-    for (const auto &stmt : ast)
+    for (auto stmt : ast)
     {
       printStmt(stmt, 1, out);
     }
     out << "]";
     return out.str();
   }
+
+  static void cleanup(vector<Stmt *> &ast)
+  {
+    for (auto stmt : ast)
+    {
+      delete stmt;
+    }
+  }
+};
+
+class ParseException : public runtime_error
+{
+public:
+  ParseException(const string &msg) : runtime_error(msg) {}
 };
 
 #endif
